ca2fd89e8ad28b31d7ef17ccb8c541c25561ed35
rafael <rafaelmat142@gmail.com>
2020-01-06 09:23:54 -0300

    feat: api github constructor and wip getUser

--- src/App.js
+++ src/App.js
@@ -1,17 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-const api = {
-  baseUrl = "https://api.github.com/",
-  client_id = 
+import React, { Component } from 'react';
+import Navbar from './Navbar';
+import axios from 'axios'
 
-}
-function App() {
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      github: {
+        url: 'https://api.github.com/users',
+        client_id: 'Iv1.deed107484be5839',
+        client_secret: '1785cee4c6ebdb265690ffc403f471a5ce265b40',
+        count: 7,
+        sort: 'created: asc' 
+      },
+      user: [],
+      repos: []
+    }
+  }
+  getUser = (e) => {
+    const user = e.target.value;
+    const{ url, client_id, client_secret, count, sort } = this.github;
+    axios
+    .get(
+      
+    )
+  }
+  render(){
   return (
     <div className="App">
-
-    </div>
+    <Navbar />
+    <input id="search" onChange="{this.getUser}" type="text" className="formControl"/>
+  </div> 
   );
+  }
 }
 
 export default App;
